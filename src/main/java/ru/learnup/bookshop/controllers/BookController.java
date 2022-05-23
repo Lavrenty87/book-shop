5e7270dee094fc0f79ad0650dd68eb71c75da64f
Lavrenty87 <Lavr19871014@yandex.ru>
2022-05-23 15:45:34 +0300

    HW 28 ok

--- src/main/java/ru/learnup/bookshop/controllers/BookController.java
+++ src/main/java/ru/learnup/bookshop/controllers/BookController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.learnup.bookshop.entity.Book2;
+import ru.learnup.bookshop.entity.Book;
+import ru.learnup.bookshop.entity.repository.BookRepository;
+import ru.learnup.bookshop.entity.service.BookService;
 
 import java.util.List;
 
@@ -16,6 +18,12 @@ import java.util.List;
 @Slf4j
 @RequestMapping("/bookController")
 public class BookController {
+    private final BookRepository repository;
+
+    public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
+
     // http://localhost:8080/books/home
     @GetMapping("/home")
     public String home(Model model) {
@@ -26,30 +34,28 @@ public class BookController {
     @GetMapping("/catalog")
     public String books(Model model) {
 
-        Book2 book = Book2.builder()
-                .count(1L)
+        List<Book> books = new BookService(repository).getBooks();
+               /* .count(1L)
                 .description("Olol")
                 .name("Book")
                 .price(100L)
                 .imageUrl("https://img2.freepng.ru/20180417/zjw/kisspng-computer-icons-literature-book-clip-art-column-5ad5a011d369f9.580985541523949585866.jpg")
-                .build();
+                .build();*/
 
         model.addAttribute(
-                "books",
-                List.of(book, book, book, book, book, book, book, book)
-        );
+                "books", books);
 
         return "books";
     }
 
     @GetMapping("/book")
     public String createPage(Model model) {
-        model.addAttribute("book", new Book2());
+        model.addAttribute("book", new Book());
         return "addBook";
     }
 
     @PostMapping("/addBook")
-    public String createBook(@ModelAttribute Book2 book, Model model) {
+    public String createBook(@ModelAttribute Book book, Model model) {
         log.debug("{}", book);
         model.addAttribute("book", book);
         return "addBook";
